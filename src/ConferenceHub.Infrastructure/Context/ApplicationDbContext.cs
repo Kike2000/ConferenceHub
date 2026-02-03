36a6c7db2667b2e5c981fb48dbc5cd0636506c69
Kike2000 <72471132+Kike2000@users.noreply.github.com>
2026-02-02 18:35:26 -0600

    feat: Add setup for db connection

---
+++ src/ConferenceHub.Infrastructure/Context/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using ConferenceHub.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace ConferenceHub.Infrastructure.Context
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base (options)
+        {            
+        }
+
+        public DbSet<Conference> Conference { get; set; }
+    }
+}
---
+++ src/ConferenceHub.Infrastructure/Migrations/20260203001244_Init.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ConferenceHub.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Conference",
+                columns: table => new
+                {
+                    ConferenceId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    PublicId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Conference", x => x.ConferenceId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Conference");
+        }
+    }
+}
