610fb75b7580a7ae686f73bde60d6be7c86dc5d4
Kike2000 <72471132+Kike2000@users.noreply.github.com>
2026-02-04 21:03:58 -0600

    feat: Initial setup for User in Infrastructure layer completed

---
+++ src/ConferenceHub.Infrastructure/Repositories/UserRepository.cs
@@ -0,0 +1,31 @@
+﻿using ConferenceHub.Domain.Entities;
+using ConferenceHub.Infrastructure.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace ConferenceHub.Infrastructure.Repositories
+{
+    public class UserRepository
+    {
+        private readonly ApplicationDbContext _context;
+        public UserRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<User>> GetUsers()
+        {
+            return await _context.User.ToListAsync();
+        }
+
+        public async Task<User> GetUserByPublicId(Guid userPublicId)
+        {
+            return await _context.User.AsNoTracking().FirstOrDefaultAsync(p => p.PublicId == userPublicId);
+        }
+
+        public async Task<bool> UpdateUser(User user)
+        {
+            _context.User.Update(user);
+            return await _context.SaveChangesAsync() > 0;
+        }
+    }
+}
