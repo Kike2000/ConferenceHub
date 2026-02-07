e9b2498562c173e513ab0ec286d9f5ec2eac31fb
Kike2000 <72471132+Kike2000@users.noreply.github.com>
2026-02-06 22:49:24 -0600

    fix: Rename column from Registration table

---
+++ src/ConferenceHub.Infrastructure/Migrations/20260207044744_UpdateFromEventToConfenceNamePropertyInRegistrationTable.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ConferenceHub.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateFromEventToConfenceNamePropertyInRegistrationTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "EventPublicId",
+                table: "Registration",
+                newName: "ConferencePublicId");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdatedAt",
+                table: "Registration",
+                type: "datetime2",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UpdatedAt",
+                table: "Registration");
+
+            migrationBuilder.RenameColumn(
+                name: "ConferencePublicId",
+                table: "Registration",
+                newName: "EventPublicId");
+        }
+    }
+}
